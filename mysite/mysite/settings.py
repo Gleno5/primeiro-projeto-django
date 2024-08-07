aa82dc98364158fd3a3c1e110a8200f333a63b9e
diogo <diogo.vinich@gmail.com>
2024-08-07 20:09:49 -0300

    ajeitando o projeto

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-x@0x_o3o+=vyntmcjgxqtc+-jek7tmy&3$!f7wzi#+_r21bu=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1", ".vercel.app"
+]
 
 
 # Application definition
@@ -68,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
